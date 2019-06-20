1c07084ed1803722b16f4c5edf2a8e7d8d33c9cd
Müller, Sascha ( SC-SRV ) <sa.mueller@dlr.de>
2019-06-20 14:37:10 +0200

    Updated version number to 4.9.0 - (Task #1)
    ---
    Task #1: Migration to GitHub

--- de.dlr.sc.virsat.model.extension.fdir.ui/src-gen/de/dlr/sc/virsat/model/extension/fdir/ui/ManifestMf.java
+++ de.dlr.sc.virsat.model.extension.fdir.ui/src-gen/de/dlr/sc/virsat/model/extension/fdir/ui/ManifestMf.java
@@ -12,7 +12,7 @@ public class ManifestMf {
 	public static final String AUTOMATIC_MODULE_NAME = "de.dlr.sc.virsat.model.extension.fdir.ui";
 	public static final String BUNDLE_SYMBOLICNAME = "de.dlr.sc.virsat.model.extension.fdir.ui";
 	public static final String BUNDLE_NAME = "VirSat Model Extension FDIR UI";
-	public static final String BUNDLE_VERSION = "4.7.0.qualifier";
+	public static final String BUNDLE_VERSION = "4.9.0.qualifier";
 	public static final String REQUIRE_BUNDLE = "de.dlr.sc.virsat.model,de.dlr.sc.virsat.project,de.dlr.sc.virsat.project.ui,de.dlr.sc.virsat.uiengine.ui,de.dlr.sc.virsat.model.extension.fdir,org.eclipse.ui,org.eclipse.ui.views.properties.tabbed,org.eclipse.core.runtime,org.eclipse.core.resources,org.eclipse.core.expressions,org.eclipse.emf.edit,org.eclipse.emf.transaction,org.eclipse.graphiti,org.eclipse.graphiti.ui,org.eclipse.elk.conn.graphiti,org.eclipse.elk.graph,org.eclipse.elk.core.service,org.eclipse.elk.alg.layered,de.dlr.sc.virsat.external.lib.jfreechart,de.dlr.sc.virsat.graphiti.ui,de.dlr.sc.virsat.graphiti,de.dlr.sc.virsat.commons.ui,de.dlr.sc.virsat.fdir.galileo,com.google.guava";
 	public static final String BUNDLE_MANIFESTVERSION = "2";
 	public static final String BUNDLE_REQUIREDEXECUTIONENVIRONMENT = "JavaSE-1.8";
