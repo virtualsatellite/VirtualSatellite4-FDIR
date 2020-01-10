552627436276198ae4e375914409a607cd5c9a13
SaMuellerDLR <sa.mueller@dlr.de>
2020-01-10 12:40:20 +0100

    First user manuel draft - (Task #221)
    - Created first user manuel draft
    - Included ascii doc into maven build process
    
    ---
    Task #221: Create User Handbook

--- bash/maven_build.sh
+++ bash/maven_build.sh
@@ -100,7 +100,7 @@ callMavenAssemble() {
 	mvn clean compile -P ${MAVEN_PROFILE},target -B -V | tee maven.log
 	echo "Check for Maven Problems on Overtarget:"
 	(grep -n "\[\(WARN\|ERROR\)\]" maven.log || exit 0  && exit 1;)
-	mvn clean install -P ${MAVEN_PROFILE},deploy,${DEPLOY_TYPE},product -B -V | tee maven.log
+	mvn clean install -P ${MAVEN_PROFILE},doc,deploy,${DEPLOY_TYPE},product -B -V | tee maven.log
 	checkforMavenProblems
 	echo "Check for AsciiDoc Problems on Product:"
 	(grep -n "\[INFO\] asciidoctor: \(WARN\|ERROR\|ERR\)" maven.log || exit 0  && exit 1;)
