4bdb4544a67c171373d522445926c18c9267b430
SaMuellerDLR <sa.mueller@dlr.de>
2020-04-04 16:48:15 +0200

    Further spotbugs fixes - (Task #266)
    ---
    Task #266: Improve static code analysis

--- de.dlr.sc.virsat.model.extension.fdir.experiments/src/de/dlr/sc/virsat/model/extension/fdir/experiments/ASynthesizerExperiment.java
+++ de.dlr.sc.virsat.model.extension.fdir.experiments/src/de/dlr/sc/virsat/model/extension/fdir/experiments/ASynthesizerExperiment.java
@@ -98,7 +98,7 @@ public class ASynthesizerExperiment {
 		
 		File[] files = folder.listFiles();
 		if (files != null) {
-			for (File file : folder.listFiles()) {
+			for (File file : files) {
 				if (file.isDirectory()) {
 					testFolder(file, folderPath + "/" + file.getName());
 				} else {
