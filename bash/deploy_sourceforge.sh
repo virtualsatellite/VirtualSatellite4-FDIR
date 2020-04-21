7679d4707066a11ef4d5249535f73386a9d38bf0
SaMuellerDLR <sa.mueller@dlr.de>
2020-04-21 18:03:44 +0200

    Fixed left over Core mention in bash script - (Task #272)
    ---
    Task #272: Fix product deployment

--- bash/deploy_sourceforge.sh
+++ bash/deploy_sourceforge.sh
@@ -49,7 +49,7 @@ uploadIntegration() {
 }
 
 uploadRelease() {
-	rsync -e ssh -avP $TRAVIS_BUILD_DIR/deploy/secured/p2/VirSat4_Core_Application/release/  dlrscmns@frs.sourceforge.net:/home/frs/project/virtualsatellite/VirtualSatellite4-Core/release/
+	rsync -e ssh -avP $TRAVIS_BUILD_DIR/deploy/secured/p2/VirSat4_FDIR_Application/release/  dlrscmns@frs.sourceforge.net:/home/frs/project/virtualsatellite/VirtualSatellite4-FDIR/release/
 }
 
 
