a2b3c49718954bffc8f881dd9249f90f2f426f4d
SaMuellerDLR <sa.mueller@dlr.de>
2020-05-28 23:48:18 +0200

    Simplfied code structure in final states minimizer - (Task #308)
    ---
    Task #308: Improve testing of Orthogonal minimization for repair and PO
    cases

--- de.dlr.sc.virsat.model.extension.fdir/src/de/dlr/sc/virsat/model/extension/fdir/recovery/minimizer/FinalStateMinimizer.java
+++ de.dlr.sc.virsat.model.extension.fdir/src/de/dlr/sc/virsat/model/extension/fdir/recovery/minimizer/FinalStateMinimizer.java
@@ -37,54 +37,56 @@ public class FinalStateMinimizer extends ARecoveryAutomatonMinimizer {
 		RecoveryAutomatonHelper raHelper = raHolder.getRaHelper();
 		
 		Set<State> finalStates = new HashSet<>();
+		List<Transition> transitionsToRemove = new ArrayList<>();
 		
 		for (State state1 : ra.getStates()) {
 			List<Transition> outgoingTransitions1 = new ArrayList<>(raHolder.getStateHolder(state1).getOutgoingTransitions());
-			if (!raHelper.isFinalState(ra, state1, outgoingTransitions1) && raHelper.isFinalStateEquivalent(ra, outgoingTransitions1)) {
+			if (raHelper.isFinalState(ra, state1, outgoingTransitions1) || !raHelper.isFinalStateEquivalent(ra, outgoingTransitions1)) {
+				continue;
+			}
 				
-				State state2 = outgoingTransitions1.get(0).getTo(); 
-				List<Transition> outgoingTransitions2 = raHolder.getStateHolder(state2).getOutgoingTransitions();
+			State state2 = outgoingTransitions1.get(0).getTo(); 
+			List<Transition> outgoingTransitions2 = raHolder.getStateHolder(state2).getOutgoingTransitions();
 				
-				if (!state1.equals(state2) && raHelper.isFinalState(ra, state2, outgoingTransitions2)) {
+			if (state1.equals(state2) || !raHelper.isFinalState(ra, state2, outgoingTransitions2)) {
+				continue;
+			}
 
-					finalStates.add(state2);
-					
-					// create new mergedState and redirect transitions 
-					List<Transition> incomingTransitions2 = new ArrayList<>(raHolder.getStateHolder(state2).getIncomingTransitions());
-					List<Transition> transitionsToRemove = new ArrayList<>();
-					for (Transition incomingTransition : incomingTransitions2) {
-						if (incomingTransition.getFrom().equals(state1)) {
-							TransitionHolder incomingTransitionHolder = raHolder.getTransitionHolder(incomingTransition);
-							if (incomingTransitionHolder.isEpsilonTransition()) {
-								transitionsToRemove.add(incomingTransition);
-							} else {
-								incomingTransitionHolder.setTo(state1);
-							}
-						}
+			finalStates.add(state2);
+			
+			// create new mergedState and redirect transitions 
+			List<Transition> incomingTransitions2 = new ArrayList<>(raHolder.getStateHolder(state2).getIncomingTransitions());
+			for (Transition incomingTransition : incomingTransitions2) {
+				if (incomingTransition.getFrom().equals(state1)) {
+					TransitionHolder incomingTransitionHolder = raHolder.getTransitionHolder(incomingTransition);
+					if (incomingTransitionHolder.isEpsilonTransition()) {
+						transitionsToRemove.add(incomingTransition);
+					} else {
+						incomingTransitionHolder.setTo(state1);
 					}
-					
-					for (Transition outgoingTransition1 : outgoingTransitions1) {
-						raHolder.getTransitionHolder(outgoingTransition1).setTo(state1);
-					}
-					
-					raHolder.removeTransitions(transitionsToRemove);
 				}
-
 			}
 			
+			for (Transition outgoingTransition1 : outgoingTransitions1) {
+				raHolder.getTransitionHolder(outgoingTransition1).setTo(state1);
+			}
 		}
 		
 		for (State state : finalStates) {
-			if (!Objects.equals(ra.getInitial(), state)) {
-				// remove the states that cannot be reached
-				List<Transition> outgoingTransitions = raHolder.getStateHolder(state).getOutgoingTransitions();
-				List<Transition> incomingTransitions = raHolder.getStateHolder(state).getIncomingTransitions();
-				if (outgoingTransitions.equals(incomingTransitions)) {
-					raHolder.removeTransitions(new ArrayList<>(outgoingTransitions));
-					raHolder.removeTransitions(new ArrayList<>(incomingTransitions));
-					raHolder.removeStates(Collections.singleton(state));
-				}
+			if (Objects.equals(ra.getInitial(), state)) {
+				continue;
+			}
+			
+			// remove the states that cannot be reached
+			List<Transition> outgoingTransitions = raHolder.getStateHolder(state).getOutgoingTransitions();
+			List<Transition> incomingTransitions = raHolder.getStateHolder(state).getIncomingTransitions();
+			if (outgoingTransitions.equals(incomingTransitions)) {
+				transitionsToRemove.addAll(outgoingTransitions);
+				transitionsToRemove.addAll(incomingTransitions);
+				raHolder.removeStates(Collections.singleton(state));
 			}
 		}
+		
+		raHolder.removeTransitions(transitionsToRemove);
 	}
 }
