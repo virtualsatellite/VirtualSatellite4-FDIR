d1bd7e0911863972772eed0e94bfcfbe6bfdab66
SaMuellerDLR <sa.mueller@dlr.de>
2020-06-13 17:07:53 +0200

    Minor UI adjustments - (Task #341)
    ---
    Task #341: Add Category for managing synthesis configuration

--- de.dlr.sc.virsat.model.extension.fdir.ui/src/de/dlr/sc/virsat/model/extension/fdir/ui/snippet/UiSnippetTableRecoveryAutomatonGen.java
+++ de.dlr.sc.virsat.model.extension.fdir.ui/src/de/dlr/sc/virsat/model/extension/fdir/ui/snippet/UiSnippetTableRecoveryAutomatonGen.java
@@ -9,6 +9,10 @@
  *******************************************************************************/
 package de.dlr.sc.virsat.model.extension.fdir.ui.snippet;
 
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.ui.forms.widgets.FormToolkit;
 
 // *****************************************************************
 // * Class Declaration
@@ -23,4 +27,16 @@ package de.dlr.sc.virsat.model.extension.fdir.ui.snippet;
  * 
  */
 public class UiSnippetTableRecoveryAutomatonGen extends AUiSnippetTableRecoveryAutomatonGen {
+	public static final int SINGLE_LINE_TABLE_HEIGHT = 35;
+	
+	@Override
+	protected Table createDefaultTable(FormToolkit toolkit, Composite sectionBody) {
+		Table table = super.createDefaultTable(toolkit, sectionBody);
+	
+		GridData gridDataTable = (GridData) table.getLayoutData();
+		gridDataTable.heightHint = SINGLE_LINE_TABLE_HEIGHT;
+		
+		table.setLayoutData(gridDataTable);
+		return table;
+	}
 }
