bcf8f2226a252fbc51d3adad707837f0d013b63e
SaMuellerDLR <sa.mueller@dlr.de>
2020-11-03 16:53:15 +0100

    Removed dead import - (Task #417)
    ---
    Task #417: Basic event failure rate does not update

--- de.dlr.sc.virsat.model.extension.fdir.ui/src/de/dlr/sc/virsat/model/extension/fdir/ui/diagram/ft/BeanPropertyDirectEditValueFeature.java
+++ de.dlr.sc.virsat.model.extension.fdir.ui/src/de/dlr/sc/virsat/model/extension/fdir/ui/diagram/ft/BeanPropertyDirectEditValueFeature.java
@@ -14,7 +14,6 @@ import org.eclipse.graphiti.features.context.IDirectEditingContext;
 import org.eclipse.graphiti.features.impl.AbstractDirectEditingFeature;
 import org.eclipse.graphiti.mm.algorithms.GraphicsAlgorithm;
 import org.eclipse.graphiti.mm.algorithms.Text;
-import org.eclipse.graphiti.mm.pictograms.ContainerShape;
 import org.eclipse.graphiti.mm.pictograms.PictogramElement;
 
 import de.dlr.sc.virsat.graphiti.util.DiagramHelper;
